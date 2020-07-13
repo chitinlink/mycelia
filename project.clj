baac8465944ab1630dc94cafb894119316ae2d63
Ada <interimgoliath@gmail.com>
2020-07-13 17:17:38 +0000

    Move to python

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject danmason/bodiam-admin-bot "0.1.0"
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [clj-http "3.10.0"]
-                 [cheshire "5.10.0"]
-                 [org.suskalo/discljord "1.0.0"]]
-  :uberjar-name "bodiam-admin-bot.jar"
-  :main bodiam-admin-bot.main)
