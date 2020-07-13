d9a4cdf7b040effd27c277d52e3b78bfa0bbbcc6
Daniel Mason <dan@juxt.pro>
2020-07-13 19:41:05 +0100

    Add test insert function.

--- lib/biggs.py
+++ lib/biggs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import logging
+import json
 
 # External dependencies
 import discord
@@ -8,12 +9,22 @@ from tinydb import TinyDB, Query
 
 log = logging.getLogger("Biggs")
 
+def check_prefix(command_prefix: str, message: discord.Message):
+  return message.content.startswith(command_prefix)
+
+def remove_prefix(command_prefix: str, message: discord.Message):
+  return message.content.replace(command_prefix, '')
+
 class Biggs(discord.Client):
   def setup(self, config: dict):
     self.run(config["token"])
-    self._tinydb_instance = TinyDB(config["tinydb_path"])
+    self.tinydb_instance = TinyDB(config["tinydb_path"])
     self._server_id = config["server_id"] # type: int
     self._notice_channel_id = config["notice_channel_id"] # type: int
+
+  def db_insert(self, message_contents: str):
+    parsed_json = json.loads(message_contents)
+    self.tinydb_instance.insert(parsed_json)
 
   async def on_ready(self):
     log.info(f"Logged on as {self.user}!")
@@ -21,6 +32,9 @@ class Biggs(discord.Client):
   async def on_message(self, message: discord.Message):
     try:
       log.info(f"Message from {message.author}: {message.content}")
+      if check_prefix("$inserttest", message):
+        self.db_insert(remove_prefix("$inserttest", message))
+
     except Exception as exc:
       log.error(exc)
       raise exc
