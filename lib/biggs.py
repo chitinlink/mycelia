6460d1598839b2d6c630568e6b210bee383665a5
Daniel Mason <dan@juxt.pro>
2020-07-14 13:28:20 +0100

    Add initial `addblacklist` and schema validation

--- lib/biggs.py
+++ lib/biggs.py
@@ -3,9 +3,11 @@
 import logging
 import json
 import re
+import sys
 
 # External dependencies
 import discord
+import jsonschema
 from tinydb import TinyDB, Query
 
 # Logging
@@ -16,8 +18,12 @@ logging.Logger.msg = msg
 
 class Biggs(discord.Client):
   def setup(self, config: dict):
-    self._tinydb_instance   = TinyDB(f"{config['tinydb_path']}db.json")
-    self._guild_id          = config["guild_id"] # type: int
+    self._tinydb_instance = TinyDB(f"{config['tinydb_path']}db.json")
+
+    self._blacklist_schema = json.load(open('blacklist_schema.json'))
+
+    self._guild_id = config["guild_id"] # type: int
+
     self._notice_channel_id = config["notice_channel_id"] # type: int
 
     self.run(config["token"])
@@ -31,9 +37,21 @@ class Biggs(discord.Client):
         removes the mention(s) and returns the remaining text. """
     return message.replace(f"<@!{self.user.id}>", "").strip()
 
-  def db_insert(self, message_contents: str):
-    parsed_json = json.loads(message_contents)
+  def db_insert(self, parsed_json):
     self._tinydb_instance.insert(parsed_json)
+
+  def add_blacklist(blacklist_json: str):
+    try:
+      # Parse JSON from string
+      parsed_json = json.loads(blacklist_json)
+      # Validate the JSON against the 'blacklist' schema - throws if invalid.
+      jsonschema.validate(instance=parsed_json, schema=self._blacklist_schema)
+      # Submit validated JSON
+      db_insert(parsed_json)
+    except:
+      e = sys.exc_info()[0]
+      log.info(f"Invalid JSON submitted, got exception: {e}")
+      raise e
 
   async def process_command(self, message: discord.Message):
     """ Take in a message and decide if it's a command,
@@ -44,10 +62,14 @@ class Biggs(discord.Client):
 
     # Select specific command
     # TODO: extract this into a separate module, or organize it some other way
-    if args[0] == "inserttest":
-      log.debug("inserttest invoked.")
-      self.db_insert(args[1])
-      await message.channel.send("Added to the database.")
+    if args[0] == "addblacklist":
+      log.debug("addblacklist invoked.")
+      try:
+        self.add_blacklist(args[1])
+        await message.channel.send(f"Added new blacklist to database")
+      except:
+        e = sys.exc_info()[0]
+        await message.channel.send(f"Failed to submit to database, got exception: {e}")
     else:
       await message.channel.send("I'm not sure what you mean.")
 
