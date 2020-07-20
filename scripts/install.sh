d57afdfbd7b9899ecb9fc86258dc85d54c057d41
Ada <interimgoliath@gmail.com>
2020-07-20 20:50:06 +0000

    systemctl link + comments etc

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-sudo ln -s ./biggs.service /etc/systemd/system/biggs.service
+# Make sure we're in /scripts/
+cd "${0%/*}"
+
+# Set everything here to executable
+chmod +x *
+
+# Soft link
+sudo systemctl link /etc/systemd/system/biggs.service
+
+# Reload and enable
+sudo systemctl daemon-reload
+sudo systemctl enable biggs.service
