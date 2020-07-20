095e43de20c8bb54dd6f0530e205e21b5a7c59c8
Ada <interimgoliath@gmail.com>
2020-07-20 20:50:06 +0000

    systemctl link + comments etc

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+# Stop and disable
+sudo systemctl stop biggs.service
+sudo systemctl disable biggs.service
+
+# Unlink
 sudo unlink /etc/systemd/system/biggs.service
+
+# Reload
+sudo systemctl daemon-reload
