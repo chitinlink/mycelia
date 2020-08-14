6da08687f596c1a827ad905a40440c840e3dc2fd
Ada <interimgoliath@gmail.com>
2020-08-14 18:53:35 +0000

    some more utils

--- lib/utils.py
+++ lib/utils.py
@@ -1,9 +1,19 @@
-def md_quote(msg: str) -> str:
-  """ Prefixes every line of given `msg` with a ">" """
-  return "> " + msg.replace("\n", "\n> ")
+def md_quote(text: str) -> str:
+  """ Prefixes every line of given `text` with a ">" """
+  return "> " + text.replace("\n", "\n> ")
 
 def md_list(lst: iter) -> str:
   """ Formats a list of strings into a consistent style """
-  out = ""
-  for i in lst: out += f"• {i}\n"
-  return out[:-1]
+  return [f"• {i}\n" for i in lst][:-1]
+
+def md_codeblock(block: str, lang: str = "") -> str:
+  """ Markdown code block """
+  return f"```{lang}\n{block}```"
+
+def md_spoiler(text: str) -> str:
+  """
+    Markdown spoiler inline block
+
+    Note issue #30.
+  """
+  return f"||{text}||"
