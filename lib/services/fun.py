b5cca5d30b24f7a7289c84fed0a46fa78d2ffff7
Ada <interimgoliath@gmail.com>
2020-10-30 22:21:32 -0100

    you can now do it to the bot and also no typing

--- lib/services/fun.py
+++ lib/services/fun.py
@@ -25,38 +25,36 @@ class Fun(Cog):
   @commands.command()
   async def owo(self, ctx: commands.Context):
     """ Small. """
-    async with ctx.typing():
-      async for msg in ctx.history(limit=1, before=ctx.message):
-        if msg.author != ctx.bot.user:
-          await ctx.message.delete()
-          owomsg = owo.owoify(msg.content)
+    async for msg in ctx.history(limit=1, before=ctx.message):
+      await ctx.message.delete()
+      owomsg = owo.owoify(msg.content)
 
-          if len(msg.embeds) > 0:
-            embeds = msg.embeds
-            for embed in embeds:
-              if embed.title:       embed.title = owo.owoify(embed.title)
-              if embed.description: embed.description = owo.owoify(embed.description)
-              if embed.footer:
-                embed.set_footer(
-                  text=owo.owoify(embed.footer.text),
-                  icon_url=embed.footer.icon_url
-                )
-              if embed.author:
-                embed.set_author(
-                  name=owo.owoify(embed.author.name),
-                  icon_url=embed.footer.icon_url
-                )
-              for f in range(len(embed.fields)):
-                embed.set_field_at(f,
-                  name=owo.owoify(embed.fields[f].name),
-                  value=owo.owoify(embed.fields[f].value)
-                )
+      if len(msg.embeds) > 0:
+        embeds = msg.embeds
+        for embed in embeds:
+          if embed.title:       embed.title = owo.owoify(embed.title)
+          if embed.description: embed.description = owo.owoify(embed.description)
+          if embed.footer:
+            embed.set_footer(
+              text=owo.owoify(embed.footer.text),
+              icon_url=embed.footer.icon_url
+            )
+          if embed.author:
+            embed.set_author(
+              name=owo.owoify(embed.author.name),
+              icon_url=embed.footer.icon_url
+            )
+          for f in range(len(embed.fields)):
+            embed.set_field_at(f,
+              name=owo.owoify(embed.fields[f].name),
+              value=owo.owoify(embed.fields[f].value)
+            )
 
-            await ctx.send(owomsg, embed=embeds[0], allowed_mentions=AllowedMentions.none())
+        await ctx.send(owomsg, embed=embeds[0], allowed_mentions=AllowedMentions.none())
 
-            if len(embeds) > 1:
-              for e in embeds[1:]:
-                await ctx.send(embed=e, allowed_mentions=AllowedMentions.none())
+        if len(embeds) > 1:
+          for e in embeds[1:]:
+            await ctx.send(embed=e, allowed_mentions=AllowedMentions.none())
 
-          elif owomsg != msg.content:
-            await ctx.send(owomsg, allowed_mentions=AllowedMentions.none())
+      elif owomsg != msg.content:
+        await ctx.send(owomsg, allowed_mentions=AllowedMentions.none())
